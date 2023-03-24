a5d4ccd851b379cb81176f585c7e591f87a93836
rivabayu <rivabays@gmail.com>
2023-03-24 15:58:34 +0700

    pembaharuan

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import EmailJS from '../components/EmailJS'
 import Profil from '../components/Profil'
+import {motion} from 'framer-motion'
+import {fadeIn} from '../Efek'
+
 
 function Contact() {
   return (
-    <div className='flex bg-hero min-h-screen pt-40 flex-col bg-center bg-auto bg-repeat'>
+    <motion.div 
+    variants={fadeIn('up',0.5)}
+    initial="hidden"
+    whileInView={'show'}div className='flex bg-hero min-h-screen pt-40 flex-col bg-center bg-auto bg-repeat'>
       <Profil/>
       <EmailJS/>
-    </div>
+    </motion.div>
   )
 }
 
